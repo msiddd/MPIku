e2a35fc8fbf56f046eabc72570a3aff848357416
msiddd <ms.idafar@gmail.com>
2024-12-07 09:17:36 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,100 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ZT5XxT5dd1":
+        Script1();
+        break;
+      case "5wnM8ciIiIJ":
+        Script2();
+        break;
+      case "5o2DgcLdEJI":
+        Script3();
+        break;
+      case "5dkzQgeqW3G":
+        Script4();
+        break;
+      case "6fQQ7vr2JoY":
+        Script5();
+        break;
+      case "5vw0CHRJUXJ":
+        Script6();
+        break;
+      case "5zECZNDdeY9":
+        Script7();
+        break;
+      case "6H81SpFna56":
+        Script8();
+        break;
+      case "6TlYUi9xtrM":
+        Script9();
+        break;
+      case "6fqq90BAemn":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.3;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume=0.8;
+}
+
